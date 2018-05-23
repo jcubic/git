b0b777c36675f118a0cdefefa6cfa14245e0bba6
Jakub Jankiewicz <jcubic@onet.pl>
2018-05-23 11:52:50 +0200

    update worker

--- sw.js
+++ sw.js
@@ -14,30 +14,24 @@
 self.addEventListener('install', function(evt) {
     self.skipWaiting();
     self.importScripts('https://cdn.jsdelivr.net/npm/browserfs');
-    (function loop() {
-        if (typeof BrowserFS == 'undefined') {
-            console.log('no browserFS');
-            setTimeout(loop, 500);
+    BrowserFS.configure({ fs: 'IndexedDB', options: {} }, function (err) {
+        if (err) {
+            console.log(err);
         } else {
-            BrowserFS.configure({ fs: 'IndexedDB', options: {} }, function (err) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    self.fs = BrowserFS.BFSRequire('fs');
-                    self.path = BrowserFS.BFSRequire('path');
-                }
-            });
+            self.fs = BrowserFS.BFSRequire('fs');
+            self.path = BrowserFS.BFSRequire('path');
         }
-    })();
+    });
 });
-var base = self.registration.scope;
-var re = new RegExp('^' + base);
 
 self.addEventListener('fetch', function (event) {
     event.respondWith(new Promise(function(resolve, reject) {
         var m = event.request.url.match(/__browserfs__(.*)/);
         if (m && self.fs) {
             var path = m[1];
+            if (path === '') {
+                path = '/';
+            }
             console.log('serving ' + path + ' from browserfs');
             fs.stat(path, function(err, stat) {
                 if (err) {
