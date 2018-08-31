4bee782d1c5bbf766d84153cc00be956530e0279
William Hilton <wmhilton@gmail.com>
2018-08-31 01:05:53 -0400

    fixes

--- js/git-worker.js
+++ js/git-worker.js
@@ -1,13 +1,13 @@
 /* global importScripts, self, BrowserFS, git, EventEmitter */
 window = self;
 importScripts(
-    //"https://unpkg.com/isomorphic-git@0.x.x",
-    '../../../iso-git-latest/dist/bundle.umd.min.js',
+    "https://unpkg.com/isomorphic-git@0.x.x",
+    // '../../../iso-git-latest/dist/bundle.umd.min.js',
     "https://unpkg.com/browserfs@1.x.x",
     "https://rawgit.com/Olical/EventEmitter/master/EventEmitter.js"
 );
 
-const localStorage = wrap('localStorage', ['getItem', 'setItem', 'deleteItem']);
+const localStorage = wrap('localStorage', ['getItem', 'setItem', 'removeItem']);
 
 function wrap(name, names) {
     const obj = {};
@@ -32,44 +32,53 @@ function wrap(name, names) {
     return obj;
 }
 
-const term = wrap('terminal', ['read', 'set_mask', 'confirm', 'resume', 'resume', 'pause', 'paused']);
+const term = wrap('terminal', ['read', 'set_mask', 'confirm', 'resume', 'pause', 'paused']);
 
-const urlify = (protocol, host, path) => `${protocol}://${host}/${path || ''}`;
-    const CredentialManager = {
-        async fill ({ protocol, host, path }) {
-            this.pased = await term.paused();
-            term.resume();
-            let url = urlify(protocol, host, path);
-            let auth = await localStorage.getItem(url);
-            if (auth) return JSON.parse(auth);
-            const username = await term.read(`Enter username for ${host}: `);
-            await term.set_mask(true);
-            const password = await term.read('Enter password: ');
-            await term.set_mask(false);
-            return {
-                username,
-                password
-            };
-        },
-        async approved({ protocol, host, path, auth }) {
-            let url = urlify(protocol, host, path);
-            const response = await term.confirm('Do you want to save this password? (Y/N) ');
-            if (response) {
-                await localStorage.setItem(url, JSON.stringify(auth));
-            }
-            if (this.paused) {
-                await term.pause();
-            }
-        },
-        async rejected({ protocol, host, path, auth }) {
-            let url = urlify(protocol, host, path);
-            if (localStorage[url]) {
-                if (await term.confirm(`Authentication to ${host} was unsuccessful. Delete saved password? (Y/N) `)) {
-                    await localStorage.deleteItem(url);
-                }
+const CredentialManager = {
+    async fill ({ url }) {
+        let paused = await term.paused();
+        if (paused) await term.resume();
+        let auth = await localStorage.getItem(url);
+        if (auth) return JSON.parse(auth);
+        const username = await term.read(`Enter username for ${new URL(url).host}: `);
+        await term.set_mask(true);
+        const password = await term.read('Enter password: ');
+        await term.set_mask(false);
+        if (paused) await term.pause();
+        return {
+            username,
+            password
+        };
+    },
+    async approved({ url, auth }) {
+        let savedAuth = await localStorage.getItem(url);
+        if (savedAuth) {
+            savedAuth = JSON.parse(savedAuth);
+            if (savedAuth.username === auth.username && savedAuth.password === auth.password) {
+                // It's already saved.
+                return
             }
         }
-    };
+        let paused = await term.paused();
+        if (paused) await term.resume();
+        const response = await term.confirm('Do you want to save this password? (Y/N) ');
+        console.log('confirm', response)
+        if (response) {
+            await localStorage.setItem(url, JSON.stringify(auth));
+        }
+        if (paused) await term.pause();
+    },
+    async rejected({ url, auth }) {
+        if (await localStorage.getItem(url)) {
+            let paused = await term.paused();
+            if (paused) await term.resume();
+            if (await term.confirm(`Authentication to ${new URL(url).host} was unsuccessful. Delete saved password? (Y/N) `)) {
+                await localStorage.removeItem(url);
+            }
+            if (paused) await term.pause();
+        }
+    }
+};
 
     
 
