e737f302fbaab640708073dd0df6d675cfb49794
Greg Agnew <kingsdistributedsystems@gmail.com>
2018-11-19 10:10:57 -0500

    Update contributors.js

--- js/contributors.js
+++ js/contributors.js
@@ -8,5 +8,10 @@ const contributors = [
         fullname: 'Brett Zamir',
         name: 'brettz9',
         url: 'http://brett-zamir.me/'
+    },
+    {
+        name: 'gagnew',
+        fullname: 'Greg Agnew',
+        url: 'https://distributed.computer'
     }
 ];
